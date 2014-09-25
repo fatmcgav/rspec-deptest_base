0e6e42bf447ba50b76cf6a930fea16dfe1a02cbd
Gavin Williams <fatmcgav@gmail.com>
2014-09-25 13:18:14 +0000

    Increase tests on deptest_base::software::glassfish class

--- spec/classes/software/glassfish_spec.rb
+++ spec/classes/software/glassfish_spec.rb
@@ -4,7 +4,12 @@ describe 'deptest_base::software::glassfish' do
 
   it { should compile }
 
-  it { should contain_group('glassfish').with_ensure('present') }
+  # Anchors
+  it { should contain_anchor('deptest_base::software::glassfish::begin') }
+  it { should contain_anchor('deptest_base::software::glassfish::end') }
+
+  it { should contain_group('glassfish').with_ensure('present').
+    that_requires('Anchor[deptest_base::software::glassfish::begin]') }
 
   it { should contain_user('glassfish').with( {
     'ensure'     => 'present',
@@ -12,6 +17,12 @@ describe 'deptest_base::software::glassfish' do
     'comment'    => 'Glassfish user account',
     'gid'        => 'glassfish'
     } ).that_requires('Group[glassfish]')
+  }
+
+  it { should contain_class('glassfish').with({ 
+    'java_ver' => 'java-7-openjdk', 
+    'manage_accounts' => false 
+    }).that_comes_before('Anchor[deptest_base::software::glassfish::end]')
   }
 
 end
